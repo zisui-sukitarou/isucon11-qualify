e2bcc8d9e02a972697ecedb330f4c68904a66718
Keigo-Yonezawa <keigo20000306@gmail.com>
2023-01-01 19:56:28 +0900

    [fix] create index

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -35,3 +35,5 @@ CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
   `url` VARCHAR(255) NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE INDEX jia_isu_uuid_index ON isu_condition(jia_isu_uuid);
