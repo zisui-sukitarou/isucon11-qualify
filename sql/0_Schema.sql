369ed4a26a249537f32419fd6f125df98e02d4d3
Keigo-Yonezawa <keigo20000306@gmail.com>
2023-01-01 16:41:34 +0900

    [fix] isu_condition index

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -37,4 +37,4 @@ CREATE TABLE `isu_association_config` (
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 
-ALTER TABLE isu_condition ADD INDEX isu_uuid_index (`jia_isu_uuid` DESC, `timestamp` DESC); 
+ALTER TABLE isu_condition ADD INDEX isu_uuid_index (`jia_isu_uuid` ASC); 
