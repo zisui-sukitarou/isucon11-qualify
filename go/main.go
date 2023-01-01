c434f416d53ea814eed1d2e938c02f8f3fd237c0
Keigo-Yonezawa <keigo20000306@gmail.com>
2023-01-01 23:47:46 +0900

    [fix] bulk insert initial data

--- go/main.go
+++ go/main.go
@@ -1200,6 +1200,7 @@ func postIsuCondition(c echo.Context) error {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
+	isuConditions := []IsuCondition{}
 	for _, cond := range req {
 		timestamp := time.Unix(cond.Timestamp, 0)
 
@@ -1207,16 +1208,23 @@ func postIsuCondition(c echo.Context) error {
 			return c.String(http.StatusBadRequest, "bad request body")
 		}
 
-		_, err = tx.Exec(
-			"INSERT INTO `isu_condition`"+
-				"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
-				"	VALUES (?, ?, ?, ?, ?)",
-			jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+		isuConditions = append(isuConditions, IsuCondition{
+			JIAIsuUUID: jiaIsuUUID,
+			Timestamp: timestamp,
+			IsSitting: cond.IsSitting,
+			Condition: cond.Condition,
+			Message: cond.Message,
+		})
+	}
+	
+	query := "INSERT INTO `isu_condition`"+
+		"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
+		"	VALUES (:jia_isu_uuid, :timestamp, :is_sitting, :condition, :message)"
 
+	_, err = tx.NamedExec(query, isuConditions)
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	err = tx.Commit()
