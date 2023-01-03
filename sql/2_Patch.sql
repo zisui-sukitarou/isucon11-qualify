e405432f2a68c2d32045a0475398fea66922640f
Keigo-Yonezawa <keigo20000306@gmail.com>
2023-01-03 22:49:35 +0900

    [fix] sql syntax err

--- sql/2_Patch.sql
+++ sql/2_Patch.sql
@@ -1,2 +1,2 @@
-UPDATE isu_condition SET level="info" WHERE condition="is_dirty=false,is_overweight=false,is_broken=false";
-UPDATE isu_condition SET level="critical" WHERE condition="is_dirty=true,is_overweight=true,is_broken=true";
+UPDATE `isu_condition` SET `level`="info" WHERE `condition`="is_dirty=false,is_overweight=false,is_broken=false";
+UPDATE `isu_condition` SET `level`="critical" WHERE `condition`="is_dirty=true,is_overweight=true,is_broken=true";
